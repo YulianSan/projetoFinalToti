5a09281874da779912d6e5889538b1c97a943717
YulianSan <knoob0733@gmail.com>
2023-09-12 19:13:47 -0300

    feat: adding routes in app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from "express"
 import dotenv from "dotenv"
+import { router } from "./router/router.js"
 
 dotenv.config()
 
@@ -13,5 +14,7 @@ app.use((_, res, next) => {
     res.setHeader('Access-Control-Allow-Headers', '*');
     next()
 })
+
+app.use(router)
 
 export { app }
