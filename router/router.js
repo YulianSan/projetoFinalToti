a73cb3eb1e2c3d6bc1e358cb6a20fabcfb314d9c
YulianSan <knoob0733@gmail.com>
2023-09-12 19:13:25 -0300

    feat: adding routes root

---
+++ router/router.js
@@ -0,0 +1,16 @@
+import express from "express"
+import { productRouter } from "./productRouter.js"
+
+const router = express.Router()
+
+router.use('/product', productRouter)
+
+router.use((err, req, res, next) => {
+    return res.status(err?.code ?? 400)
+        .json({ 
+            success: false, 
+            message: err?.message ?? 'Error' 
+        })
+})
+
+export { router }
