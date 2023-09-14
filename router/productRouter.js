c4a3da99d78fb38a9b6dacd12a51470e23963d9c
YulianSan <knoob0733@gmail.com>
2023-09-14 18:31:56 -0300

    feat: basic validate create product

--- router/productRouter.js
+++ router/productRouter.js
@@ -2,11 +2,18 @@ import express from "express"
 import { createProduct } from "../controller/product/createProduct.js"
 import { getProducts } from "../controller/product/getProducts.js";
 import { getProduct } from "../controller/product/getProduct.js";
+import { validate } from "../helpers/validate.js";
+import { z as zod } from "zod";
 
 const productRouter = express.Router()
 
+const validateCreateProduct = zod.object({
+    name: zod.string().min(1),
+    price: zod.number().gt(0),
+})
+
 productRouter.get('/', getProducts)
 productRouter.get('/:id', getProduct)
-productRouter.post('/', createProduct);
+productRouter.post('/', validate({ body: validateCreateProduct }), createProduct);
 
 export { productRouter }
