1faf2545e0ce6f4dbefd7d724387c6511723b0c0
YulianSan <knoob0733@gmail.com>
2023-09-14 20:08:41 -0300

    feat(user): adding controller to handle with login

---
+++ controller/user/login.js
@@ -0,0 +1,37 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+import { sequelize } from '../../database/connection.js'
+import { QueryTypes } from 'sequelize'
+import { compare } from 'bcrypt'
+import jwt from 'jsonwebtoken'
+
+export const login = async (req, res, next) => {
+    const { email, password } = req.body
+
+    const user = (await sequelize.query(`
+        SELECT * FROM users 
+            where email = :email
+        `, { 
+            type: QueryTypes.SELECT,
+            replacements: { email }
+        }
+    ))[0]
+
+    const passwordIsRight = await compare(password, user.password)
+
+    if(!passwordIsRight){
+        next({
+            message: 'Access denied',
+            code: 401,
+        })
+    }
+
+    const token = jwt.sign({ 
+        id: user.id, 
+        email: user.email, 
+        name: user.name 
+    }, process.env.SECRET_KEY)
+
+    return res.json({ token, success: true })
+}
