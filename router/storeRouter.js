cc6f756ab10e5c572b231260d98f8cc929c60533
YulianSan <knoob0733@gmail.com>
2023-09-23 18:02:55 -0300

    feat: basic router and validate

--- router/storeRouter.js
+++ router/storeRouter.js
@@ -1,32 +1,32 @@
 import express from 'express'
+import { login } from '../controller/store/login.js'
 import { z as zod } from 'zod'
 import { validate } from '../helpers/validate.js'
-import { login } from '../controller/user/login.js'
-import { createUser } from '../controller/user/createUser.js'
+import { createStore } from '../controller/store/createStore.js'
 
-const userRouter = express.Router()
+const storeRouter = express.Router()
 
 const validateLogin = zod.object({
     email: zod.string().min(1),
     password: zod.string().min(1),
 })
 
-const validateCreateUser = zod.object({
+const validateCreate = zod.object({
     name: zod.string().min(1),
     email: zod.string().min(1),
     password: zod.string().min(1),
 })
 
-userRouter.post(
+storeRouter.post(
     '/login', 
     validate({ body: validateLogin }), 
     login
 )
 
-userRouter.post(
-    '/user/create', 
-    validate({ body: validateCreateUser }), 
-    createUser
+storeRouter.post(
+    '/', 
+    validate({ body: validateCreate }), 
+    createStore
 )
 
-export { userRouter }
+export { storeRouter }
