80e3911816b2356fa99674ebeb113be2dd898300
YulianSan <knoob0733@gmail.com>
2023-09-23 23:21:24 -0300

    fix(product): get only id and name of store

--- src/controller/product/getProduct.js
+++ src/controller/product/getProduct.js
@@ -5,9 +5,9 @@ export const getProduct = async (req, res, next) => {
     const { id } = req.params
 
     const product = await Product.findByPk(id, {
-        include: [Store]
+        include: [{ model: Store, attributes: ['name', 'id'] }]
     })
-    
+
     if( !product ) return next({ code: 404, message: 'Product not found'})
 
     return res.json({ success: true, data: product })
