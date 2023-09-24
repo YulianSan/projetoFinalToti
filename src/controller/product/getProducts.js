353929900a8ad7bd8927c453849dda99a4842f8e
YulianSan <knoob0733@gmail.com>
2023-09-23 23:22:00 -0300

    feat(products): slice array in small array with 15 of length

--- src/controller/product/getProducts.js
+++ src/controller/product/getProducts.js
@@ -2,5 +2,12 @@ import { Product } from "../../model/Product.js"
 
 export const getProducts = async (req, res) => {
     const products = await Product.findAll() 
-    return res.json({ success: true, data: products })
+
+    const productsChunk = products.reduce((acc, product, index) => {
+        let currentChunk = Math.floor(index/15)
+        acc[currentChunk] = [...(acc?.[currentChunk] ?? []), product]
+        return acc;
+    }, [])
+
+    return res.json({ success: true, data: productsChunk })
 }
